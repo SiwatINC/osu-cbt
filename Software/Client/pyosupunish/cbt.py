727bea47f05c438894c0f7b9bb7da4ce4e68e54f
Siwat Sirichai <siwat@siwatinc.com>
2022-01-17 00:26:49 +0700

    add LED support

--- Software/Client/pyosupunish/cbt.py
+++ Software/Client/pyosupunish/cbt.py
@@ -17,9 +17,12 @@ class cbt:
         self.mcu = pymata4.Pymata4()
         self.mcu.set_pin_mode_digital_output(pin_number=config.FRONT_VALVE_PIN)
         self.mcu.set_pin_mode_digital_output(pin_number=config.REAR_VALVE_PIN)
+        self.mcu.set_pin_mode_digital_output(pin_number=config.LED_PIN)
+        self.mcu.digital_pin_write(pin=config.LED_PIN,value=1)
         self.mcu.set_pin_mode_digital_input_pullup(pin_number=config.RETRACT_BUTTON_PIN,callback=self.handle_retract_button)
         self.mcu.set_pin_mode_digital_input_pullup(pin_number=config.EXTEND_BUTTON_PIN,callback=self.handle_extend_button)
         self.mcu.set_pin_mode_digital_input_pullup(pin_number=config.TEST_BUTTON_PIN,callback=self.handle_test_button)
+        
         self.logger.setLevel(config.CBT_LOGGING_LEVEL)
     def handle_retract_button(self, data):
         if (perf_counter()-self.debounce_timer >= 0.5) and data[2] == 0: #data at index 2 is the pin value.
